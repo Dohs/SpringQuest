599149bb566cccb67da9c34ee7476472f265d876
Yacine Mansouri <yacin.mansouri@yahoo.fr>
2019-10-24 15:04:17 +0200

    quest02

---
+++ src/main/java/com/wildcodeschool/myProject/Doctor.java
@@ -0,0 +1,29 @@
+package com.wildcodeschool.myProject;
+
+public class Doctor {
+
+    private int id;
+    private String name;
+
+    public Doctor(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
--- src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
+++ src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
@@ -2,12 +2,50 @@ package com.wildcodeschool.myProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
+@Controller
 @SpringBootApplication
 public class MyProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectApplication.class, args);
+    }
 
+    @RequestMapping("/doctor/{id}")
+    @ResponseBody
+    public Doctor hello(@PathVariable Integer id) {
+
+        if (id >=  9 && id <= 13){
+            String name = "";
+            switch(id) {
+                case 9 :
+                    name= "William Hartnell";
+                    break;
+                case 10:
+                    name= "Patrick Troughton";
+                    break;
+                case 11:
+                    name = "Jon Pertwee";
+                    break;
+                case 12:
+                    name = "Tom Baker";
+                    break;
+                case 13:
+                    name = "Peter Davison";
+            }
+            return new Doctor(name,id);
+        }
+        if (id >= 1 && id <= 8){
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See Other");
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Question Not Found");
+        }
+    }
 }
